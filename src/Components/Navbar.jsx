0471d0c3f06c8fcdcd0a77ace87b2a2afda37911
Emmanuel Oyibo <75739167+dabsmanuel@users.noreply.github.com>
2022-05-26 02:13:35 +0100

    first commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,34 +1,32 @@
-import React from 'react';
+import React from "react";
 import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div className="nav">
-      <nav>
-        <ul>
-          <li>
-            <a href="#biography">Biography</a>
-          </li>
+    <nav className="nav">
+      <ul className="nav_menu">
+        <li className="nav__item">
+          <a href="#biography">Biography</a>
+        </li>
 
-          <li>
-            <a href="#tribute" className="active">
-              Tribute Page
-            </a>
-          </li>
+        <li className="nav__item">
+          <a href="#tribute" className="active">
+            Tribute Page
+          </a>
+        </li>
 
-          <li>
-            <a href="#discography">Discography</a>
-          </li>
-        </ul>
+        <li className="nav__item">
+          <a href="#discography">Discography</a>
+        </li>
+      </ul>
 
-        <div className="Hamburger">
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>
-      </nav>
-    </div>
+      <div className="nav__toggler">
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
+      </div>
+    </nav>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
