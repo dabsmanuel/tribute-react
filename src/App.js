36a461a8d0e9d858d65c1824ab0086d5664c5bd7
Emmanuel Oyibo <75739167+dabsmanuel@users.noreply.github.com>
2022-05-30 02:42:43 +0100

    added media querry to biography image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Navbar from './Components/Navbar';
 import HeaderText from './Components/HeaderText';
 import Biography from './Components/Biography';
-import pic1 from './images/image 5 (1).png';
-import pic2 from './images/image 5 (2).png';
-import pic3 from './images/image 5 (3).png';
-import pic4 from './images/image 5 (4).png';
-import pic5 from './images/image 5 (5).png';
-import pic6 from './images/image 5.png';
+// import pic1 from './images/image 5 (1).png';
+// import pic2 from './images/image 5 (2).png';
+// import pic3 from './images/image 5 (3).png';
+// import pic4 from './images/image 5 (4).png';
+// import pic5 from './images/image 5 (5).png';
+// import pic6 from './images/image 5.png';
 
 
 function App() {
