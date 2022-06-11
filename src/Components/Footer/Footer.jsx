b4d45fc551b579cbfe642d8079bef5992f8e6a08
Emmanuel Oyibo <75739167+dabsmanuel@users.noreply.github.com>
2022-06-11 18:28:54 +0100

    added margin to header text

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
-import './Footer.css'
-
+import './Footer.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 const Footer = () => {
+
+  const heart = <FontAwesomeIcon icon={faHeart} />;
   return (
     <footer>
       <main className="foot">
         <p>&copy; 2022 - Tribute Page</p>
+        <p>built by Dabs with  <span>{heart}</span></p>
       </main>
     </footer>
   );
